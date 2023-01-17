62e07a14f7e988bc011f97cd00984be4e6cb8418
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-17 15:38:47 +0100

    Feature: Create objects to index page (#17)
    * Imported ArticleOverview
    
    * Created objects for the articles

---
+++ js/modules/Article-overview.js
@@ -0,0 +1,49 @@
+export default function ArticleOverview() {
+	const articles1 = [
+		{
+			header: 'Anhalt University of Applied Sciences',
+			title: 'Dessau Summer School of Architecture',
+			image: '/assets/images/alan-rostovtev.jpg',
+			link: '/article.html',
+		},
+		{
+			header: 'ArkDes',
+			title: 'Joar Nango: Girjegumpi',
+			image: '/assets/images/mohammad-mohsen-rohani.jpg',
+			link: '/article.html',
+		},
+		{
+			header: 'Sabu Kohso',
+			title: 'Nuclear Recursivity and Seismic Awakening',
+			image: '/assets/images/albert-stoynov.jpg',
+			link: '/article.html',
+		},
+		{
+			header: 'Bauhaus Dessau Foundation',
+			title: 'Bauhaus Lab 2023: Concrete Antarctic',
+			image: '/assets/images/brice-cooper.jpg',
+			link: '/article.html',
+		},
+	]
+
+	const articles2 = [
+		{
+			header: 'Radiowaves Collective',
+			title: 'Mothering a Movement: Notes from India’s Longest Anti-Nuclear Struggle',
+			image: '/assets/images/alan-rostovtev-article.jpg',
+			link: '/article.html',
+		},
+		{
+			header: 'Livia Krohn Miller',
+			title: 'Ten Thousand Years of Isolation',
+			image: '/assets/images/refargotohp.jpg',
+			link: '/article.html',
+		},
+		{
+			header: 'AA editorial',
+			title: 'The Avant-Garde Museum',
+			image: '/assets/images/tingfeng-xiav.jpg',
+			link: '/article.html',
+		},
+	]
+}
