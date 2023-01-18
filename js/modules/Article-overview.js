ef6ecb65c0fdc43af74172a1ed705773b540b37c
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-01-18 12:25:44 +0100

    Feature: Render objects on index page (#38)
    * Created a function to render one html article
    
    * Added loop over object to render articles
    
    * Corrected image links in object
    
    * Added container for articles
    
    * Styled containers
    
    * Render both article objects and refactored
    
    * Added alternative text
    
    * Changed class name
    
    * Changed to correct class name when render
    
    Co-authored-by: Ole Kristian Heian Olaisen <olekristianolaisen@gmail.com>

--- js/modules/Article-overview.js
+++ js/modules/Article-overview.js
@@ -1,27 +1,34 @@
 export default function ArticleOverview() {
+	const articlesOverview1Container = document.querySelector('.article-overview__1-container');
+	const articlesOverview2Container = document.querySelector('.article-overview__2-container');
+
 	const articles1 = [
 		{
 			header: 'Anhalt University of Applied Sciences',
 			title: 'Dessau Summer School of Architecture',
 			image: '/assets/images/alan-rostovtev.jpg',
+			alt: 'White wall',
 			link: '/article.html',
 		},
 		{
 			header: 'ArkDes',
 			title: 'Joar Nango: Girjegumpi',
 			image: '/assets/images/mohammad-mohsen-rohani.jpg',
+			alt: 'White building',
 			link: '/article.html',
 		},
 		{
 			header: 'Sabu Kohso',
 			title: 'Nuclear Recursivity and Seismic Awakening',
 			image: '/assets/images/albert-stoynov.jpg',
+			alt: 'Angled building',
 			link: '/article.html',
 		},
 		{
 			header: 'Bauhaus Dessau Foundation',
 			title: 'Bauhaus Lab 2023: Concrete Antarctic',
 			image: '/assets/images/brice-cooper.jpg',
+			alt: 'Building with bridge',
 			link: '/article.html',
 		},
 	]
@@ -30,20 +37,86 @@ export default function ArticleOverview() {
 		{
 			header: 'Radiowaves Collective',
 			title: 'Mothering a Movement: Notes from India’s Longest Anti-Nuclear Struggle',
-			image: '/assets/images/alan-rostovtev-article.jpg',
+			image: '/assets/images/alan-rostovtev-article.png',
+			alt: 'Glass building',
 			link: '/article.html',
 		},
 		{
 			header: 'Livia Krohn Miller',
 			title: 'Ten Thousand Years of Isolation',
 			image: '/assets/images/refargotohp.jpg',
+			alt: 'White building',
 			link: '/article.html',
 		},
 		{
 			header: 'AA editorial',
 			title: 'The Avant-Garde Museum',
-			image: '/assets/images/tingfeng-xiav.jpg',
+			image: '/assets/images/tingfeng-xia.jpg',
+			alt: 'Bridge',
 			link: '/article.html',
 		},
 	]
+
+	renderHTML();
+
+	function renderHTML() {
+		renderArticles();
+	}
+
+	function renderArticles() {
+		for (const oneArticle of articles1) {
+			const article = createArticle(oneArticle.link);
+			const image = createImage(oneArticle.image, oneArticle.alt); 
+			article.append(image);
+			const card = createCard(oneArticle.header, oneArticle.title);
+			article.append(card);
+			articlesOverview1Container.append(article);
+		}
+
+		for (const oneArticle of articles2) {
+			const article = createArticle(oneArticle.link);
+			const image = createImage(oneArticle.image, oneArticle.alt); 
+			article.append(image);
+			const card = createCard(oneArticle.header, oneArticle.title);
+			article.append(card);
+			articlesOverview2Container.append(article);
+		}
+
+		function createArticle(link) {
+			const article = document.createElement('div');
+			article.className = 'article-overview';
+			article.href = link;
+
+			return article;
+		}
+
+		function createImage(source, alt) {
+			const imageContainer = document.createElement('div');
+			imageContainer.className = 'article-overview__image';
+
+			const image = document.createElement('img');
+			image.src = source;
+			image.alt = alt;
+			imageContainer.append(image);
+			
+			return imageContainer;
+		}
+
+		function createCard(header, title) {
+			const card = document.createElement('div');
+			card.className = 'article-overview__card';
+	
+			const cardHeader = document.createElement('p');
+			cardHeader.className = 'article-overview__header';
+			cardHeader.innerText = header;
+	
+			const cardTitle = document.createElement('p');
+			cardTitle.className = 'article-overview__title';
+			cardTitle.innerText = title;
+			card.append(cardHeader);
+			card.append(cardTitle);
+			
+			return card;
+		}
+	}
 }
