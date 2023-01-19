47cb1401e10c823a7797a1eede388cb9ea7a158b
OlekOlaisen <olekristianolaisen@gmail.com>
2023-01-19 22:16:43 +0100

    Made navigation bar sticky to top on scroll

---
+++ js/modules/header.js
@@ -0,0 +1,20 @@
+export default function Header() {
+
+window.onscroll = function() {stickyNavigation()};
+
+let header = document.querySelector(".header__content");
+let content = document.querySelector(".header__content--sticky");
+
+let sticky = header.offsetTop;
+
+function stickyNavigation() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("header__content--sticky")
+	 content.style.right = '-1rem';
+  } else {
+    header.classList.remove("header__content--sticky");
+  }
+}
+
+
+}
