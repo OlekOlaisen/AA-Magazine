9fbb3a8523d057e7ad475c4b17354a891761f758
Ole Kristian Heian Olaisen <olekristianolaisen@gmail.com>
2023-01-20 10:27:28 +0100

    Made navigation bar sticky to top on scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import ArticleOverview from './modules/Article-overview.js';
 import Hamburger from './modules/hamburgermenu.js';
+import Header from './modules/header.js';
 
 ArticleOverview();
+Header();
 Hamburger();
