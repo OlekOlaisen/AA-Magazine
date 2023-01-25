f08455755d2c7a60326448f02fd3ea6d73d23b5e
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-01-25 14:36:50 +0100

    formatted subscribe.js

--- js/modules/subscribe.js
+++ js/modules/subscribe.js
@@ -1,4 +1,5 @@
 export default function Subscribe() {
+	
 	const subscribeButtonContainer = document.querySelector('.subscribe');
 	const closeButton = document.querySelector('.subscribe__close-button');
 
