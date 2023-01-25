fbeefdead9fd44db9880c2f392b0d61db957460a
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-01-25 14:33:46 +0100

    created js for closing subscribe button

---
+++ js/modules/subscribe.js
@@ -0,0 +1,8 @@
+export default function Subscribe() {
+	const subscribeButtonContainer = document.querySelector('.subscribe');
+	const closeButton = document.querySelector('.subscribe__close-button');
+
+	closeButton.addEventListener('click', function() {
+		subscribeButtonContainer.style.display = "none";
+	});
+}
