42f8529396120401dcd356f9a4adfcef7e4046be
Mads-Emil Fellkjær <55183287+Madsemilf@users.noreply.github.com>
2023-01-25 14:57:26 +0100

    Added subscribe button to index
    * added subscribe button to index
    
    * created css for subscribe button
    
    * created js for closing subscribe button
    
    * formatted subscribe.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ import ArticleOverview from './modules/Article-overview.js';
 import Hamburger from './modules/hamburgermenu.js';
 import Header from './modules/header.js';
 import Events from './modules/events.js';
+import Subscribe from './modules/subscribe.js';
 
 ArticleOverview();
 Header();
 Hamburger();
 Events();
+Subscribe();
