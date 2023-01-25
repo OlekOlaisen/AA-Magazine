255af002cef4e83fd4b81a236d4b57406319094b
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-01-25 21:12:31 +0100

    added button text on desktop layout

--- js/modules/subscribe.js
+++ js/modules/subscribe.js
@@ -1,6 +1,12 @@
 export default function Subscribe() {
 	
 	const subscribeButtonContainer = document.querySelector('.subscribe');
+	const subscribeButton = document.querySelector('.subscribe__button')
+	
+	if (window.innerWidth > 1100) {
+		subscribeButton.innerHTML = 'Subscribe for architecture, art, and more <span class="subscribe__close-button">X</span>';
+  	}
+
 	const closeButton = document.querySelector('.subscribe__close-button');
 
 	closeButton.addEventListener('click', function() {
