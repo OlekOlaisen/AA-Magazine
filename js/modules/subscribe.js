8d67ae7679898358dd0e59bf7fd9359780f4ef93
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-01-27 11:14:36 +0100

    made function to smoothly change inner text on button

--- js/modules/subscribe.js
+++ js/modules/subscribe.js
@@ -3,9 +3,17 @@ export default function Subscribe() {
 	const subscribeButtonContainer = document.querySelector('.subscribe');
 	const subscribeButton = document.querySelector('.subscribe__button')
 	
-	if (window.innerWidth > 1100) {
-		subscribeButton.innerHTML = 'Subscribe for architecture, art, and more <span class="subscribe__close-button"></span>';
-  	}
+	window.addEventListener('resize', renderSubscribeButton);
+
+	function renderSubscribeButton() {
+		if (window.innerWidth > 768) {
+			subscribeButton.innerHTML = 'Subscribe for architecture, art, and more <span class="subscribe__close-button"></span>';
+		} else {
+			subscribeButton.innerHTML = 'Subscribe <span class="subscribe__close-button"></span>';
+		}
+	};
+	
+	renderSubscribeButton();
 
 	const closeButton = document.querySelector('.subscribe__close-button');
 
