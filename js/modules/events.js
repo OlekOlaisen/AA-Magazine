db795ede1cccac1c49e063d17572f41bdfad7fbd
Ole Kristian Heian Olaisen <olekristianolaisen@gmail.com>
2023-01-29 19:53:23 +0100

    Fixed events and styling

--- js/modules/events.js
+++ js/modules/events.js
@@ -1,109 +1,91 @@
 export default function Events() {
-	
-	let events = [
-		{
-			"title": "Conversation: Sami Sami sacred mountains and sacrificial stones",
-			"category": "Lectures",
-			"date": "January 20",
-			"time": "12:00-14:00"
-		},
-		{
-			"title": "Free guided tour in Kunna Guanna Concha",
-			"category": "Exhibitions",
-			"date": "February 5",
-			"time": "16:00-17:00"
-		},
-		{
-			"title": "Exhibition opening: Metahaven and Susanne M. Winterling",
-			"category": "Exhibitions",
-			"date": "February 12",
-			"time": "18:00"
-		},
-		{
-			"title": "Monumental love – Art historian Marit Paasche on Hannah Ryggen",
-			"category": "Open Seminars",
-			"date": "March 4",
-			"time": "18:00-20:00"
-		}
-	]; 
-
-	// Repeats the events array
-	let repeatedEvents = events;
-
-		for (let event = 0; event < 2; event++) {
-			repeatedEvents = repeatedEvents.concat(events);
-		};
-
-	let sidebar = document.querySelector('.sidebar__events');
-
-    //Creates the container for the buttons
-    let buttonContainer = document.createElement('div');
-    buttonContainer.className = "sidebar__events-buttons"
-    sidebar.appendChild(buttonContainer);
-
-    //Creates the event filter buttons
-    let lecturesButton = document.createElement('button');
-    lecturesButton.innerText = "Lectures";
-    lecturesButton.className = "sidebar__events-button";
-    lecturesButton.addEventListener('click', () => {
-        toggleFilter("Lectures");
-    });
     
-    let exhibitionsButton = document.createElement('button');
-    exhibitionsButton.innerText = "Exhibitions";
-    exhibitionsButton.className = "sidebar__events-button";
-    exhibitionsButton.addEventListener('click', () => {
-        toggleFilter("Exhibitions");
-    });
-    
-    let openSeminarsButton = document.createElement('button');
-    openSeminarsButton.innerText = "Open Seminars";
-    openSeminarsButton.className = "sidebar__events-button";
-    openSeminarsButton.addEventListener('click', () => {
-        toggleFilter("Open Seminars");
-    });
-
-    buttonContainer.appendChild(lecturesButton);
-    buttonContainer.appendChild(exhibitionsButton);
-    buttonContainer.appendChild(openSeminarsButton);
-    
-    //Defines the toggleFilter function
-    function toggleFilter(category) {
-        let filteredEvents = repeatedEvents.filter(event => event.category === category);
-        if (filteredEvents.length === eventsToDisplay.length && eventsToDisplay.every((events,index)=> events === filteredEvents[index])) {
-            //if all events are already displayed
-            renderEvents(repeatedEvents);
-            eventsToDisplay = repeatedEvents;
-        } else {
-            //if the events are filtered
-            renderEvents(filteredEvents);
-            eventsToDisplay = filteredEvents;
+    let events = [
+        {
+            "title": "Conversation: Sami Sami sacred mountains and sacrificial stones",
+            "category": "Lectures",
+            "date": "January 20",
+            "time": "12:00-14:00"
+        },
+        {
+            "title": "Free guided tour in Kunna Guanna Concha",
+            "category": "Exhibitions",
+            "date": "February 5",
+            "time": "16:00-17:00"
+        },
+        {
+            "title": "Exhibition opening: Metahaven and Susanne M. Winterling",
+            "category": "Exhibitions",
+            "date": "February 12",
+            "time": "18:00"
+        },
+        {
+            "title": "Monumental love – Art historian Marit Paasche on Hannah Ryggen",
+            "category": "Open Seminars",
+            "date": "March 4",
+            "time": "18:00-20:00"
         }
-    };
-
-    //Displays the events in HTML
-    let eventsToDisplay = repeatedEvents;
-    function renderEvents(eventsToDisplay) {
-        sidebar.innerHTML = "";
-        sidebar.appendChild(buttonContainer);
-        for (let event of eventsToDisplay) {
-            let eventDiv = document.createElement('div');
-            eventDiv.className = "sidebar__events-event"
-            eventDiv.innerHTML = `
-                <p class="sidebar__events-event-category">${event.category}</p>
-                <div class="sidebar__events-date-time">
-                    <p class="sidebar__events-event-date">${event.date}</p>
-                    <p class="sidebar__events-event-time">${event.time}</p>
-                </div>
-                <h2 class="sidebar__events-event-title">${event.title}</h2>
-                
-            `;
-            sidebar.appendChild(eventDiv);
+    ]; 
+    
+    events = events.concat(events).concat(events);
+    
+    let currentFilter = null;
+    
+    const filterEvents = document.querySelector(".sidebar__events");
+    const filterEventButtons = document.querySelectorAll(".sidebar__events-toggle");
+    
+    for (const toggle of filterEventButtons) {
+        toggle.addEventListener('click', handleFilterEventButtonsClick);
+    }
+    
+    function handleFilterEventButtonsClick(event) {
+        toggleEventFilter(event.target.dataset.filter);
+        renderEvents();
+    }
+    
+    function toggleEventFilter(filter) {
+        if (currentFilter === filter) {
+            currentFilter = null;
+        }   else {
+            currentFilter = filter;
         }
-    };
+    }
+    
+    function returnFilteredEvents() {
+        if (currentFilter === null) {
+            return events;
+        }   else {
+            return events.filter(event => event.category === currentFilter);
+        }
+    }
 
-    //Displays all events by default
-    renderEvents(repeatedEvents);
-
-}
-
+    renderEvents();
+    
+    function renderEvents() {
+        filterEvents.innerHTML = '';
+        
+        for (const toggle of filterEventButtons) {
+            toggle.classList.remove('sidebar__events-toggle--active');
+            if(toggle.dataset.filter === currentFilter) {
+                toggle.classList.add('sidebar__events-toggle--active');
+            }
+        }
+        
+        for (const event of returnFilteredEvents()) {
+            const filterEvent = document.createElement('div');
+            
+            filterEvent.dataset.category = event.category;
+            filterEvent.className = 'sidebar__event';
+            filterEvent.innerHTML = `
+            <p class="sidebar__events-event-category">${event.category}</p>
+            <div class="sidebar__events-event-date-time">
+            <p class="sidebar__events-event-date">${event.date}</p>
+            <p class="sidebar__events-event-time">${event.time}</p>
+            </div>
+            <h2 class="sidebar__events-event-title">${event.title}</h2>
+            `
+            
+            filterEvents.appendChild(filterEvent);
+        }
+    }
+}    
